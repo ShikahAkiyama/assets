29c3dbf0c8499029a833e0d643aff0b3ca690abe
Shikah Akiyama <suliyanediharsono@gmail.com>
2026-03-20 09:52:24 +0700

    Add Statcounter tracking code to footer1.js

--- 01/footer1.js
+++ 01/footer1.js
@@ -1,1 +1,17 @@
-
+<!-- Default Statcounter code for
+https://ebay.com/ -->
+<script type="text/javascript">
+var sc_project=13211700; 
+var sc_invisible=1; 
+var sc_security="fe9b87ec"; 
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js"
+async></script>
+<noscript><div class="statcounter"><a title="Web Analytics"
+href="https://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="https://c.statcounter.com/13211700/0/fe9b87ec/1/"
+alt="Web Analytics"
+referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+<!-- End of Statcounter Code -->
